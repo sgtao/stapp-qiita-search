09adfe7c510255d0137069b21d82bfe54fbe799b
Shogo Ogami <sg.tao.so@gmail.com>
2024-08-15 16:30:55 +0900

    Fix:update qiita-article-search for split get_qiita... to functions

--- src/pages/11_qiita-article-search.py
+++ src/pages/11_qiita-article-search.py
@@ -1,34 +1,7 @@
 # pages/11_qiita-article-search.py
-from urllib.parse import urlencode
-
 import streamlit as st
-import requests
 
-# Qiita APIのベースURL
-BASE_URL = "https://qiita.com/api/v2"
-
-
-def get_qiita_articles(endpoint, params=None):
-    """
-    Qiita APIから記事を取得するための共通関数。
-
-    指定されたエンドポイントとオプションのパラメータを使用して、
-    Qiita APIからデータを取得します。
-
-    Args:
-        endpoint (str): APIのエンドポイントを指定します。
-        params (dict, optional): クエリパラメータを含む辞書。デフォルトはNoneです。
-
-    Returns:
-        list or dict: APIからのレスポンスをJSON形式で返します。
-    """
-    if params:
-        query_string = urlencode(params)
-        url = f"{BASE_URL}/{endpoint}?{query_string}"
-    else:
-        url = f"{BASE_URL}/{endpoint}"
-    response = requests.get(url)
-    return response.json()
+from functions.api_qiita_articles import get_qiita_articles
 
 
 def main():
