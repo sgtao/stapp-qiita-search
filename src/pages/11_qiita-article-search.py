4b9ce2f13e500f696c3419c43b15b677e376a2c5
Shogo Ogami <sg.tao.so@gmail.com>
2024-08-15 15:38:17 +0900

    Fix:update qiita-article-search page for replace radio button on sidemenu

--- src/pages/11_qiita-article-search.py
+++ src/pages/11_qiita-article-search.py
@@ -8,8 +8,20 @@ import requests
 BASE_URL = "https://qiita.com/api/v2"
 
 
-# 共通のAPIリクエスト関数
 def get_qiita_articles(endpoint, params=None):
+    """
+    Qiita APIから記事を取得するための共通関数。
+
+    指定されたエンドポイントとオプションのパラメータを使用して、
+    Qiita APIからデータを取得します。
+
+    Args:
+        endpoint (str): APIのエンドポイントを指定します。
+        params (dict, optional): クエリパラメータを含む辞書。デフォルトはNoneです。
+
+    Returns:
+        list or dict: APIからのレスポンスをJSON形式で返します。
+    """
     if params:
         query_string = urlencode(params)
         url = f"{BASE_URL}/{endpoint}?{query_string}"
@@ -19,18 +31,23 @@ def get_qiita_articles(endpoint, params=None):
     return response.json()
 
 
-# Streamlitアプリの構築
 def main():
-    st.title("Qiita Article Search App")
+    """Streamlitアプリの構築"""
 
-    selected_menu = st.radio(
-        "Pick one:", ["最新記事一覧", "キーワード検索", "記事ID指定表示"]
-    )
-    st.subheader(f"selected menu: {selected_menu}")
+    # サイドバーにメニューを配置
+    with st.sidebar:
+        selected_menu = st.radio(
+            "メニューを選択:",
+            ["最新記事一覧", "キーワード検索", "記事ID指定表示"],
+        )
+
+    # サイドバーにメニューを配置
+    st.title("Qiita Article Search App")
+    # st.subheader(f"selected menu: {selected_menu}")
 
     # 最新記事の表示
     if selected_menu == "最新記事一覧":
-        st.header("最新記事一覧")
+        st.subheader("最新記事一覧")
         if st.button("表示・更新"):
             st.session_state.latest_articles = get_qiita_articles("items")
 
@@ -42,7 +59,7 @@ def main():
 
     # キーワード検索
     elif selected_menu == "キーワード検索":
-        st.header("キーワード検索")
+        st.subheader("キーワード検索")
         keyword = st.text_input("キーワードを入力してください")
         if st.button("検索"):
             st.session_state.search_results = get_qiita_articles(
@@ -57,12 +74,19 @@ def main():
 
     # 記事ID指定表示
     elif selected_menu == "記事ID指定表示":
-        st.header("記事ID指定表示")
-        item_id = st.text_input("記事IDを入力してください")
-        if st.button("表示"):
+        st.subheader("記事ID指定表示")
+        if "shown_item_id" not in st.session_state:
+            st.session_state.shown_item_id = ""
+
+        item_id = st.text_input(
+            label="記事IDを入力してください",
+            value=st.session_state.shown_item_id,
+        )
+        if item_id != st.session_state.shown_item_id:
             st.session_state.shown_article = get_qiita_articles(
                 f"items/{item_id}"
             )
+            st.session_state.shown_item_id = item_id
 
         if "shown_article" in st.session_state:
             article = st.session_state.shown_article
